17b26f956f132a4201882238965870b56bfec47d
herneshem <ernestoami@hotmail.com>
2023-04-19 11:14:30 +0200

    Cantante Moderna

---
+++ src/app/paginados/stylemusic.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-stylemusic',
+    templateUrl: './stylemusic.component.html',
+    styleUrls: ['stylemusic.component.css', '../app.component.css']
+
+
+})
+export class stylemusicComponent{
+
+    
+}
