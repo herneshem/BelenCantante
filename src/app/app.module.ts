6bb80433733e9dae00f90db6b03ce890dd334873
herneshem <ernestoami@hotmail.com>
2023-04-19 23:35:39 +0200

    estiloscantante resp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MusicComponent } from './paginados/music.component';
 import { FormularioComponent } from './paginados/formulario.component';
-
-
+import { stylemusicComponent } from './paginados/stylemusic.component';
+import { SpiritmusicComponent } from './paginados/spiritmusic.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MusicComponent,
     FormularioComponent,
+    stylemusicComponent,
+    SpiritmusicComponent
 
   ],
   imports: [
